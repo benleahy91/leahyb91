da5439601d5a4e498918e9ccb8cef4fabb836e9f
benleahy91 <benleahy91@gmail.com>
2018-07-21 17:12:41 -0400

    hangman push

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,225 +1,125 @@
-// Global Variables
+//Global Variables
 var dogCelebs = ['Woof Blitzer', 'Bark Cuban', 'Mick Wagger', 'Julio Bones', 'Alex Ofetchkin', 'Jason Beagle', 'Hillary Ruff', 'Pet Farvre', 'Waggie Gyllenhall'];
-var randomWord = '';
-var wordSplit = '';
-var letter = '';
-var keyPress = '';
-var keyCode = '';
-var keyLog = '';
-var alphabet = '';
 var wins = 0;
-var guessesLeft = 0;
 var losses = 0;
-var underscores = '';
-var guessedLetters = '';
-var wordLength = '';
-
-
 
 
 function startGame() {
 
-	// Define Variables
-	guessesLeft = 12;
-	wordSplit = [];
-	underscores = [];
-	guessedLetters = [];
-	wordLength = randomWord.length;
-	alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
+	var guessesLeft = 12;
+	var wordSplit = [];
+	var guessWord = [];
+	var wrongGuesses = [];
 
 	// Pick Word
-	randomWord = dogCelebs[Math.floor(Math.random() * dogCelebs.length)];
-
-	// Split
-	wordSplit = randomWord.split('');
+	var randomWord = dogCelebs[Math.floor(Math.random() * dogCelebs.length)];
 	
+	// Split
+	var wordSplit = randomWord.split('');
+
 	//Underscores
 	wordSplit = dogCelebs[Math.floor(Math.random() * dogCelebs.length)];
 
 	wordSplit = wordSplit.replace(/\s/g, '');
 
-
-	console.log(wordSplit);
-
-
 	for (var i = 0; i < wordSplit.length; i++) {
-		underscores.push('_');
+		guessWord.push('_');
 	}
 
-	underscores = underscores.join('   ');
+		guessWord = guessWord.join('   ');
 
-	console.log(underscores);
-
-	document.getElementById("underscores").innerHTML = (underscores);
-
+	//Guess Word
+	document.getElementById("guessWord").innerHTML = (guessWord);
 	// Letters Guessed
-	document.getElementById('guessedLetters').innerHTML = (guessedLetters);
-
+	document.getElementById('wrongGuesses').innerHTML = (wrongGuesses);
 	//Guesses Remaining
 	document.getElementById('guessesLeft').innerHTML = (guessesLeft);
-
 	// Wins
 	document.getElementById('wins').innerHTML = (wins);
-
 	// Losses
 	document.getElementById('losses').innerHTML = (losses);
 
 }
 
-
 function playGame (event) {
 
-	// var keyPress = String.fromCharCode(event.keyCode).toUpperCase();
-	// var keyCode = event.keyCode; ***why did this have to be in the onkeyup function?***
-	keyLog = [];
-	wordSplit = '';
-	keyPress = '';
-	wrongGuesses = '';
-	guessedLetters = [];
-	alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	var guessesLeft = 12;
+	var wordSplit = [];
+	var guessWord = [];
+	var wrongGuesses = [];
 
-
-
-	// Start function on HTML
-	document.addEventListener ('keyPress', playGame, false);
-
-	document.getElementById('keyPress').innerHTML = keyPress;
+	var randomWord = dogCelebs[Math.floor(Math.random() * dogCelebs.length)];
 	
-	document.getElementById('keyCode').innerHTML = keyCode;
+	var wordSplit = randomWord.split('');
 
+	wordSplit = dogCelebs[Math.floor(Math.random() * dogCelebs.length)];
 
-	document.onkeyup = function(event) {
+	wordSplit = wordSplit.replace(/\s/g, '');
 
-	keyPress = String.fromCharCode(event.keyCode).toUpperCase();
-	keyCode = event.keyCode;
-	alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-	keyLog;
-	guessedLetters;
+	for (var i = 0; i < wordSplit.length; i++) {
+		guessWord.push('_');
+	}
 
-//start Jared
+	guessWord = guessWord.join('   ');
 
-		// check if letter
-			for (var i = 0; i < alphabet.length + 1; i++)  {
+	console.log(wordSplit);
+	console.log(guessWord);
 
-			if (keyPress.indexOf(alphabet[i]) >= 0) {
-	  			keyLog.push(keyPress);
-	  		}
-		  
-				// else {
-					// console.log("You're not guessing numbers dummy! That would be a really stupid game.")
-				// }
-			}
+	function checkLetter (letter) {
 
-		// console.log(keyLog)
+		document.addEventListener ('keyPress', playGame, false);
 
-		//check if guessed
-		for (var i = 0; i <= keyLog.length - 1; i++) {
-			if (keyPress.indexOf(keyLog[i]) <= 0) {
-				guessedLetters.push(keyPress);
-			}
-			else {
-				// console.log('You already guessed that letter.')
+		document.getElementById('keyPress').innerHTML = keyPress;
+	
+		document.getElementById('keyCode').innerHTML = keyCode;
+
+		document.onkeyup = function(event) {
+	
+			var keyCode = event.keyCode;
+			var keyPress = String.fromCharCode(event.keyCode).toUpperCase();
+			var letterInWord = false;
+			var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+			console.log(keyPress);
+
+			for (var i = 0; i < wrongGuesses.length; i++) {
+				if (alphabet.indexOf(keyPress) === -1) {
+					console.log('That is not a letter');
+				}
+
+				else if (keyPress === wrongGuesses[i]) {
+					letterInWord === true;
+					checkGuess();
+				}
 			}
 		}
-				console.log(guessedLetters)
-
-		// console.log(keyLog)
-
-
-// end Jared
-
-		// for (var i = 0; i < wordSplit.length + 1; i++) {
-		// 	if (keyPress.indexOf(wordSplit) <= 0) {
-		// 		// console.log("Guess another letter.");
-		// 		(guessedLetters.push(keyPress]);
-		// 	}
-		// 			console.log(guessedLetters)
-		// 			console.log(keyPress)
-
-		// }
 	}
 }
-				// underscore.replace(underscore[k].CharAt(keyPress[i]), keyPress[i])			
+// 	function checkGuess (e) {
+// 		for (var i = 0; i < wordSplit.length -1; i++)
 
-
-// 		// check if contained in word split // replace underscore with letter // track guesses remaining // loss condition
-		// for (var i = 0; i < wordSplit.length + 1; i++) {
-		// 	if (keyPress[i] === wordSplit[j]) { 
-		// 	 underscore.replace(underscore[k].CharAt(keyPress[i]), keyPress[i])
-		// 	}
-
-// 			else {
-// 				(guessesLeft = guessesLeft - 1)
-// 			}
-// 		}
 // 	}
 // }
 
-// function endGame (event) {
+	// for (var i = 0; i < wordSplit.length; i++) {
+	// 	if (letterInWord === true) {
+	// 		guessWord.push(keyPress)
+	// 	}
+	// }
 
-
-// 	if (guessesLeft = 0) {
-// 		(losses = losses + 1);
-// 		alert("You lose. Your defeat will be etched in the annals of history and your great, great, great, grandchildren will be ashamed to call you their ancestor becfore you are inevitably forgotten.");
-// 	}
-		
-// 	for (var i === 0; i < underscores.length + 1, i++) {
-// 		if (underscores[i] !== "_") {
-// 			(wins = wins + 1)
-// 			alert('You have fought the good fight and your blood has enriched the yard of Ned Flanders');
-// 		}
-// 	}
-// }
-
-// function resetGame (event) {
-
-// 	for (var i === 0; i < underscores.length + 1, i++) {
-// 	if (underscores[i] !== "_") {
-// 		prompt('Would you like to play again?');
-// 			if (prompt = true) {
-// 				startGame();
-// 			}
-// 			else {
-// 				alert("Fine, I don't want you playing my game anyway. I put a lot of work into this. I honestly hope your butt falls off.")
-// 			}
-// 		}
-// 	}
-
-// 	if (guessesLeft === 0) {
-// 		prompt('Would you like to play again?');
-
-// 		if (prompt = true) {
-// 			startGame();
-// 		}
-// 		else {
-// 			alert('You reek of fear and your sons will be feckless cucks. Begone, coward.');
-// 		}
-
-// 	}
-
-// }
-
-	
-  // End Game
-  // if guesses === 0 then prompt user Want to play again?
-
-  // if playAgain === true 
-  // 	startGame();
-  // else "Thanks For Paying!";
 
 
 
 startGame();
 playGame();
-// endGame();
-// resetGame();
-
-// start game sets parameters
-//function for listening to keylogs and checking to see if letter check to see if it's been type, check to see if it matches
-//win/loss condition
-// reset game
 
 
+// start game
+// play game
+	// check letter
+	// if (letter is in alphaet) {
+	// 	if { letter is not in guessedLetters
+	// 		push keyPress to lettersGuessed
+	// 		guess(letter);
+	//
 
-// Win/Loss Counter
